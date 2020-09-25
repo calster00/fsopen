f484a57a0ceb8b913bb1bede3794a0f58c3e2857
calster00 <63422553+calster00@users.noreply.github.com>
2020-09-25 17:34:55 +0300

    part2 ex2.3

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -6,10 +6,9 @@ const Header = ({ course }) => {
 };
 
 const Total = ({ course }) => {
-  const sum =
-    course.parts[0].exercises +
-    course.parts[1].exercises +
-    course.parts[2].exercises;
+  const sum = course.parts.reduce((sum, part) => {
+    return sum + part.exercises;
+  }, 0);
   return <p>Number of exercises {sum}</p>;
 };
 
@@ -24,46 +23,47 @@ const Part = (props) => {
 const Content = ({ course }) => {
   return (
     <div>
-      {course.parts.map(part =>
-        <Part key={part.id} part={part}/>
-      )}
+      {course.parts.map((part) => (
+        <Part key={part.id} part={part} />
+      ))}
     </div>
   );
 };
 
-const Course = ({course}) => {
+const Course = ({ course }) => {
   return (
     <div>
-      <Header course={course}/>
-      <Content course={course}/>
+      <Header course={course} />
+      <Content course={course} />
+      <Total course={course} />
     </div>
-  )
-}
+  );
+};
 
 const App = () => {
   const course = {
     id: 1,
-    name: 'Half Stack application development',
+    name: "Half Stack application development",
     parts: [
       {
-        name: 'Fundamentals of React',
+        name: "Fundamentals of React",
         exercises: 10,
-        id: 1
+        id: 1,
       },
       {
-        name: 'Using props to pass data',
+        name: "Using props to pass data",
         exercises: 7,
-        id: 2
+        id: 2,
       },
       {
-        name: 'State of a component',
+        name: "State of a component",
         exercises: 14,
-        id: 3
-      }
-    ]
-  }
+        id: 3,
+      },
+    ],
+  };
 
-  return <Course course={course} />
-}
+  return <Course course={course} />;
+};
 
 ReactDOM.render(<App />, document.getElementById("root"));
