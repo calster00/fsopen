742cf34aefeca3f0908b674ebb6fc322263681da
calster00 <63422553+calster00@users.noreply.github.com>
2020-09-28 18:58:29 +0300

    part2 ex2.13 (buggy)

--- part2/rest-countries/src/components/CountryList.js
+++ part2/rest-countries/src/components/CountryList.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState } from "react";
+import CountryInfo from "./CountryInfo";
 
-const CountryList = ({list}) => {
+const CountryList = ({ list }) => {
+  const [info, setInfoState] = useState(-1);
+
+  const displayInfo = (index) => setInfoState(index);
+
   return (
     <div style={{ listStyle: "none" }}>
-      {list.map((country) => 
-        <li key={country.numericCode}>{country.name}</li>)}
+      {list.map((country, index) => (
+        <li key={index}>
+          {country.name}
+          <button onClick={() => displayInfo(index)}>info</button>
+        </li>
+      ))}
+      {info > -1 && <CountryInfo country={list[info]} />}
     </div>
   );
 };
