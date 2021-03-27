a014e57444d1bcea9ca62adda8701ea306689dbf
calster00 <63422553+calster00@users.noreply.github.com>
2021-03-27 20:00:35 +0200

    part4 ex4.19

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,6 +1,15 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+
+const getTokenFrom = (request) => {
+  const authorization = request.get("authorization");
+  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+    return authorization.substring(7);
+  }
+  return null;
+};
 
 blogsRouter.get("/", async (request, response, next) => {
   try {
@@ -17,7 +26,13 @@ blogsRouter.get("/", async (request, response, next) => {
 
 blogsRouter.post("/", async (request, response, next) => {
   try {
-    const user = await User.findOne({ username: "root" });
+    const token = getTokenFrom(request);
+    const decodedToken = jwt.verify(token, process.env.SECRET);
+    if (!token || !decodedToken.id) {
+      return response.status(401).json({ error: "token missing or invalid" });
+    }
+
+    const user = await User.findById(decodedToken.id);
     const blog = new Blog({ ...request.body, user: user._id });
     const savedBlog = await blog.save();
 
